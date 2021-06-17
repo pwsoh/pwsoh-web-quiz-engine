107e69ce65aa82f5bfeff2454c0f74afd9308d3e
pwsoh <86034835+pwsoh@users.noreply.github.com>
2021-06-17 11:35:06 +0800

    Initialize user related files

--- user/UserService.java
+++ user/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
+
     private final UserRepository users;
 
     public UserService(UserRepository users) {
