d4efdc35d4b49e98eb2030b2c49ab1f62e4551f6
pwsoh <86034835+pwsoh@users.noreply.github.com>
2021-06-17 11:27:55 +0800

    Add spring security config

---
+++ security/Security.java
@@ -0,0 +1,44 @@
+package engine.security;
+
+import engine.user.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public Security(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/register").permitAll()
+                .antMatchers("/actuator/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .httpBasic();
+    }
+}
